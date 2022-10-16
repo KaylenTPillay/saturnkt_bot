9312ec325cc98576a5f4a2c6ab872930d3f483e0
Kaylen Travis Pillay <kaylen.pillay@softwareological.com>
2022-10-16 12:50:53 +0200

    Member Join Greeting
    1.Added simple embed message greeting

--- event/memberjoin/controlmemberjoin.py
+++ event/memberjoin/controlmemberjoin.py
@@ -1,3 +1,5 @@
+from discord import Embed
+from discord import Colour
 
 class EventControllerMemberJoin:
     ''' This class controls the on_member_join event from discord '''
@@ -6,17 +8,25 @@ class EventControllerMemberJoin:
         self.greeting_channel_name = greeting_channel_name
 
     async def handleEvent(self, member):
-        await self.__perform_greeting(self.greeting_channel_name, member)
-        # TODO: Ensure the member has the correct role.
-        # TODO: Send the member a private message informing them of this bots commands
+        channel_name = self.greeting_channel_name
+        await self.__perform_greeting(channel_name, member)
 
     async def __perform_greeting(self, channel_name, member):
         channel = self.__get_greeting_guild_channel(channel_name, member)
         if (channel == None):
             return
 
-        await channel.send(f'Hello {member.display_name}!')
+        await channel.send(embed=self.__generate_welcome_embed_greeting(member.display_name))
+
 
     def __get_greeting_guild_channel(self, channel_name, member):
         guild_channels = member.guild.channels
         return next((channel for channel in guild_channels if channel.name == channel_name), None)
+
+    def __generate_welcome_embed_greeting(slef, member_name):
+        embed_colour = Colour.dark_purple()
+        embed_title = 'Welcome to the Guild!'
+        embed_description = f'Thanks for joining us {member_name}'
+
+        embed_model = Embed(title=embed_title, description=embed_description, colour=embed_colour)
+        return embed_model
